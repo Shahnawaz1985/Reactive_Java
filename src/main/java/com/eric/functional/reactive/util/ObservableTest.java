0573a70ed440aeba2b2800c76c6baaa0dc469b0f
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-01-11 20:59:43 +0530

    Updated code.

--- src/main/java/com/eric/functional/reactive/util/ObservableTest.java
+++ src/main/java/com/eric/functional/reactive/util/ObservableTest.java
@@ -2,6 +2,11 @@ package com.eric.functional.reactive.util;
 
 import io.reactivex.Observable;
 
+/**
+ * 
+ * @author Shahnawaz
+ *
+ */
 public class ObservableTest {
 	
 	public static void main(String[] args) {
@@ -9,11 +14,14 @@ public class ObservableTest {
 		Observable.range(5, 3).
 		subscribe(i -> { log("Element : "+i); });
 		log("After");
+		
+		Observable<Integer> it = Observable.just(23);
+		it.subscribe(a -> System.out.println("Testing Observable.just() : "+a));
 	}
 	
 	private static void log(String message) {
 		System.out.println(Thread.currentThread().getName()+":"+message);
 	}
-	
 
 }
+
