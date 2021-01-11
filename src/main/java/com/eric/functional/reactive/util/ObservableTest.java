b5d7add3034b680001b9526b65e19079331fd92d
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-01-11 21:39:46 +0530

    Deriving natural numbers with Observables.

--- src/main/java/com/eric/functional/reactive/util/ObservableTest.java
+++ src/main/java/com/eric/functional/reactive/util/ObservableTest.java
@@ -1,6 +1,9 @@
 package com.eric.functional.reactive.util;
 
+import java.math.BigInteger;
+
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 
 /**
  * 
@@ -25,11 +28,38 @@ public class ObservableTest {
 		System.out.println("-------------------------------------------");
 		it.subscribe(a -> log("Element A :"+a));
 		it.subscribe(a -> log("Element B :"+a));
+		
+		System.out.println("-------------------------------------------");
+		
+		Observable<BigInteger> naturalNumbers = Observable.create(subscriber -> {
+			Runnable runnable = () -> {
+				BigInteger i = BigInteger.ZERO;
+				while(!subscriber.isDisposed()) {
+					subscriber.onNext(i);
+					i = i.add(BigInteger.ONE);
+				}
+			};
+			Thread th = new Thread(runnable);
+			th.setName("Natural Number printing thread -> ");
+			th.start();
+			
+		});
+		
+		Disposable disposable = naturalNumbers.subscribe(x -> log("Number : "+x));
+		
+		try {			
+			Thread.sleep(1000);			
+		} catch (InterruptedException ex) {			
+			
+			ex.printStackTrace();
+		}
+		
+		System.out.println("Thread Interruption triggered!");
+		disposable.dispose();
 	}
 	
 	private static void log(String message) {
 		System.out.println(Thread.currentThread().getName()+":"+message);
 	}
-
 }
 
